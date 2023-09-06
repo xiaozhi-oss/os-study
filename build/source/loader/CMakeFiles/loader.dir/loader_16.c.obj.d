2971fd0b7ecae4c11127fd76653282c05eee1eea
小智 <1815101896@qq.com>
2023-09-06 11:01:42 +0800

    内存检测功能完成

--- build/source/loader/CMakeFiles/loader.dir/loader_16.c.obj.d
+++ build/source/loader/CMakeFiles/loader.dir/loader_16.c.obj.d
@@ -1,2 +1,6 @@
 source/loader/CMakeFiles/loader.dir/loader_16.c.obj: \
- D:/Work/CWork/os-study/diy-x86os-master/start/start/source/loader/loader_16.c
+ D:/Work/CWork/os-study/diy-x86os-master/start/start/source/loader/loader_16.c \
+ D:/Work/CWork/os-study/diy-x86os-master/start/start/source/loader/loader.h \
+ D:/Work/CWork/os-study/diy-x86os-master/start/start/source/comm/boot_info.h \
+ D:/Work/CWork/os-study/diy-x86os-master/start/start/source/comm/types.h \
+ D:/Work/CWork/os-study/diy-x86os-master/start/start/source/comm/types.h
--- source/loader/loader.h
+++ source/loader/loader.h
@@ -4,4 +4,21 @@
 #ifndef LOADER_H
 #define LOADER_H
 
+#include "comm/boot_info.h"
+#include "comm/types.h"
+
+// 内存检测信息结构
+typedef struct SMAP_entry {
+    uint32_t BaseL; // base address uint64_t
+    uint32_t BaseH;
+    uint32_t LengthL; // length uint64_t
+    uint32_t LengthH;
+    uint32_t Type; // entry Type
+    uint32_t ACPI; // extended
+}__attribute__((packed)) SMAP_entry_t;  
+/* 
+    __attribute__((packed)) 
+        用于告诉编译器不要在结构体的成员之间插入任何填充字节，
+        以确保结构体在内存中是以紧凑的形式存储的。
+*/
 #endif
